075b5b62d5982f6d847d88c3bf53a69bbd0c7973
Quan Nguyen <quan.nguyen@ryte.com>
2018-11-12 22:23:16 +0700

    no message

--- src/carousel/carousel.js
+++ src/carousel/carousel.js
@@ -4,19 +4,19 @@ import './carousel.css'
 
 const items = [
   {
-    src: 'https://images-na.ssl-images-amazon.com/images/G/01/img18/events/blackfriday/landingpage/cdbf_desktop_1500x300_v3.jpg',
+    src: 'https://www.citypassguide.com/media/slideshow/furniture-shop-3.jpg',
     altText: 'Slide 1',
     caption: 'Slide 1',
     header: 'Slide 1 Header'
   },
   {
-    src: 'https://images-na.ssl-images-amazon.com/images/G/01/dex/2018/CFS/Dex_CFS_GW_DesktopHero_1500x600_1x._CB480800597_.jpg',
+    src: 'https://www.citypassguide.com/media/slideshow/furniture-shop-5.jpg',
     altText: 'Slide 2',
     caption: 'Slide 2',
     header: 'Slide 2 Header'
   },
   {
-    src: 'https://images-na.ssl-images-amazon.com/images/G/01/kindle/merch/2018/CAMPAIGN/Holiday/November-1/TAB/gateway/Karnak-Tall-GW-3000x1200-2X._CB480591846_.jpg',
+    src: 'https://www.citypassguide.com/media/slideshow/furniture-shop-6.jpg',
     altText: 'Slide 3',
     caption: 'Slide 3',
     header: 'Slide 3 Header'
